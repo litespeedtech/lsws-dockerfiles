6b37a4b083a75815efb7ea7ab731d20e4e1e0acc
Cold-Egg <eleu@litespeedtech.com>
2020-02-28 16:42:21 -0500

    update entrypoint

--- template/entrypoint.sh
+++ template/entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-if [ -z "$(ls -A -- "/usr/local/lsws/conf/")" ]; then
-	cp -R /usr/local/lsws/.conf/* /usr/local/lsws/conf/
+LSDIR='/usr/local/lsws'
+if [ -z "$(ls -A -- "${LSDIR}/conf/")" ]; then
+	cp -R ${LSDIR}/.conf/* ${LSDIR}/conf/
 fi
-if [ -z "$(ls -A -- "/usr/local/lsws/admin/conf/")" ]; then
-	cp -R /usr/local/lsws/admin/.conf/* /usr/local/lsws/admin/conf/
+if [ -z "$(ls -A -- "${LSDIR}/admin/conf/")" ]; then
+	cp -R ${LSDIR}/admin/.conf/* ${LSDIR}/admin/conf/
 fi
-chown 999:999 /usr/local/lsws/conf -R
-chown 999:1000 /usr/local/lsws/admin/conf -R
+if [ -z ${LSDIR}/conf/serial.no ] && [ -z ${LSDIR}/conf/license.key ]; then
+    rm -f ${LSDIR}/conf/trial.key*
+    wget -P ${LSDIR}/conf http://license.litespeedtech.com/reseller/trial.key
+fi
+
+chown 999:999 ${LSDIR}/conf -R
+chown 999:1000 ${LSDIR}/admin/conf -R
 $@
 while true; do
-	if ! /usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running with PID *' > /dev/null; then
+	if ! ${LSDIR}/bin/lswsctrl status | grep 'litespeed is running with PID *' > /dev/null; then
 		break
 	fi
 	sleep 60
